59d0bfd4af2b0ed68d649e26da498a0b53d8d75d
Shreya Gupta <guptashreya5905@gmail.com>
2025-09-07 22:10:14 +0530

    Create 1-TrappingRainWater.c

---
+++ Leetcode/1-TrappingRainWater.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h> 
+int trap(int* height, int heightSize) {
+    if (heightSize == 0) {
+        return 0;
+    }
+
+    int left = 0;
+    int right = heightSize - 1;
+    int leftMax = 0;
+    int rightMax = 0;
+    int totalWater = 0;
+
+    while (left < right) {
+        if (height[left] < height[right]) {
+            if (height[left] >= leftMax) {
+                leftMax = height[left];
+            } else {
+                totalWater += leftMax - height[left];
+            }
+            left++;
+        } else {
+            if (height[right] >= rightMax) {
+                rightMax = height[right];
+            } else {
+                totalWater += rightMax - height[right];
+            }
+            right--;
+        }
+    }
+    return totalWater;
+}
