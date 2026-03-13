c6f2b229b0e42a731138c82ca51ab70165d3091d
Shreya Gupta <guptashreya5905@gmail.com>
2026-03-13 12:49:31 +0530

    Create 3296-MinimumNumberOfSecondsToMakeMountainHeightZero.py

---
+++ Leetcode/3296-MinimumNumberOfSecondsToMakeMountainHeightZero.py
@@ -0,0 +1,28 @@
+import math
+
+class Solution:
+    def minNumberOfSeconds(self, mountainHeight: int, workerTimes: list[int]) -> int:
+        
+        def can_finish(t):
+            total = 0
+            
+            for w in workerTimes:
+                x = int(math.sqrt(2*t/w + 0.25) - 0.5)
+                total += x
+                
+                if total >= mountainHeight:
+                    return True
+            
+            return False
+        
+        left, right = 1, 10**16
+        
+        while left < right:
+            mid = (left + right) // 2
+            
+            if can_finish(mid):
+                right = mid
+            else:
+                left = mid + 1
+        
+        return left
