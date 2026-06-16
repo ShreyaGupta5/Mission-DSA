3861e754143c292f939dfc9066404947e0621267
Shreya Gupta <guptashreya5905@gmail.com>
2026-06-16 18:59:50 +0530

    Create 3612–ProcessStringwithSpecialOperationsI.py

---
+++ Leetcode/3612–ProcessStringwithSpecialOperationsI.py
@@ -0,0 +1,16 @@
+class Solution:
+    def processStr(self, s: str) -> str:
+        res = []
+
+        for ch in s:
+            if ch.isalpha():
+                res.append(ch)
+            elif ch == "*":
+                if res:
+                    res.pop()
+            elif ch == "#":
+                res.extend(res)
+            elif ch == "%":
+                res.reverse()
+
+        return "".join(res)
