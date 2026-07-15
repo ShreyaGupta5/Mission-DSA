d8fd185b68b2b77fd1516249bfd535ca90ebd4b1
Shreya Gupta <guptashreya5905@gmail.com>
2026-07-15 22:26:36 +0530

    Create 3658-GCDOfOddAndEvenSums.java

---
+++ Leetcode/3658-GCDOfOddAndEvenSums.java
@@ -0,0 +1,9 @@
+class Solution {
+   
+    public int gcdOfOddEvenSums(int n) {
+        // The mathematical formula simplifies to returning n directly
+        // This works because of the specific relationship between
+        // the sum formulas for consecutive odd and even numbers
+        return n;
+    }
+}
