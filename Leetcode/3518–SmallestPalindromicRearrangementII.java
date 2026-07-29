f0bbab3138cb8354ec7fc786df5f1077730f2fea
Shreya Gupta <guptashreya5905@gmail.com>
2026-07-29 21:03:48 +0530

    Create 3518–SmallestPalindromicRearrangementII.java

---
+++ Leetcode/3518–SmallestPalindromicRearrangementII.java
@@ -0,0 +1,84 @@
+import java.util.*;
+
+class Solution {
+
+    private static final long LIMIT = 1_000_001L;
+
+    public String smallestPalindrome(String s, int k) {
+        int[] freq = new int[26];
+
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++;
+        }
+
+        char mid = 0;
+        int[] half = new int[26];
+        int halfLen = 0;
+
+        for (int i = 0; i < 26; i++) {
+            if ((freq[i] & 1) == 1) {
+                mid = (char) ('a' + i);
+            }
+            half[i] = freq[i] / 2;
+            halfLen += half[i];
+        }
+
+        if (countWays(half, halfLen) < k) {
+            return "";
+        }
+
+        StringBuilder left = new StringBuilder();
+
+        for (int pos = 0; pos < halfLen; pos++) {
+            for (int ch = 0; ch < 26; ch++) {
+                if (half[ch] == 0) continue;
+
+                half[ch]--;
+                long ways = countWays(half, halfLen - pos - 1);
+
+                if (ways >= k) {
+                    left.append((char) ('a' + ch));
+                    break;
+                } else {
+                    k -= ways;
+                    half[ch]++;
+                }
+            }
+        }
+
+        StringBuilder ans = new StringBuilder(left);
+
+        if (mid != 0) ans.append(mid);
+
+        ans.append(new StringBuilder(left).reverse());
+
+        return ans.toString();
+    }
+
+    private long countWays(int[] cnt, int total) {
+        long res = 1;
+
+        for (int c : cnt) {
+            if (c == 0) continue;
+            res *= comb(total, c);
+            if (res >= LIMIT) return LIMIT;
+            total -= c;
+        }
+
+        return res;
+    }
+
+    private long comb(int n, int r) {
+        if (r > n) return 0;
+        r = Math.min(r, n - r);
+
+        long ans = 1;
+
+        for (int i = 1; i <= r; i++) {
+            ans = ans * (n - r + i) / i;
+            if (ans >= LIMIT) return LIMIT;
+        }
+
+        return ans;
+    }
+}
