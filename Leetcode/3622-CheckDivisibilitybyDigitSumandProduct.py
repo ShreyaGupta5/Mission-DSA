fe0f0fed160d754c26ecc76286ef9470bc7cc583
Shreya Gupta <guptashreya5905@gmail.com>
2026-08-22 09:41:06 +0530

    Create 3622-CheckDivisibilitybyDigitSumandProduct.py

---
+++ Leetcode/3622-CheckDivisibilitybyDigitSumandProduct.py
@@ -0,0 +1,18 @@
+class Solution:
+    def checkDivisibility(self, n: int) -> bool:
+        digit_sum = 0
+        digit_product = 1
+        
+        x = n
+        
+        while x > 0:
+            digit = x % 10
+            
+            digit_sum += digit
+            digit_product *= digit
+            
+            x //= 10
+        
+        total = digit_sum + digit_product
+        
+        return n % total == 0
