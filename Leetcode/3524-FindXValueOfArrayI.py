c3be65f77d53c0b1ad88eb309baf79f6e2cbe912
Shreya Gupta <guptashreya5905@gmail.com>
2026-09-21 10:01:52 +0530

    Create 3524-FindXValueOfArrayI.py

---
+++ Leetcode/3524-FindXValueOfArrayI.py
@@ -0,0 +1,25 @@
+class Solution:
+    def resultArray(self, nums, k):
+        ans = [0] * k
+        dp = [0] * k
+
+        for num in nums:
+            x = num % k
+
+            new_dp = [0] * k
+
+            # Start a new subarray with only num
+            new_dp[x] += 1
+
+            # Extend previous subarrays
+            for r in range(k):
+                new_r = (r * x) % k
+                new_dp[new_r] += dp[r]
+
+            # Add current subarrays to answer
+            for r in range(k):
+                ans[r] += new_dp[r]
+
+            dp = new_dp
+
+        return ans
