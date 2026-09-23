b8643cb40f91b32d70a2b08dc6d6dd7db4301a1b
Shreya Gupta <guptashreya5905@gmail.com>
2026-09-23 11:23:33 +0530

    Create 1658-MinimumOperationsToReduceXToZero.py

---
+++ Leetcode/1658-MinimumOperationsToReduceXToZero.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def minOperations(self, nums, x):
+        target = sum(nums) - x
+        if target < 0:
+            return -1
+        ans = -1
+        left = s = 0
+        for right, x in enumerate(nums):
+            s += x
+            while s > target:
+                s -= nums[left]
+                left += 1
+            if s == target:
+                ans = max(ans, right - left + 1)
+        return -1 if ans < 0 else len(nums) - ans
